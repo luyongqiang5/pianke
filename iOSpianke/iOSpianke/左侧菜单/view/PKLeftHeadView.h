7facf19aaf89c96f98e1a862672738d382488dcc
luyongqiang5 <2545123654@qq.com>
2016-01-15 14:51:50 +0800

    测边栏完整

--- iOSpianke/iOSpianke/左侧菜单/view/PKLeftHeadView.h
+++ iOSpianke/iOSpianke/左侧菜单/view/PKLeftHeadView.h
@@ -10,14 +10,14 @@
 
 @interface PKLeftHeadView : UIView
 
-@property (strong, nonatomic)       UIButton *iconImageBtn; // 头像按钮
-@property (strong, nonatomic)       UIButton *userNameBtn; // 用户名登陆按钮
-@property (strong, nonatomic)       UIButton *downBtn;  //  下载按钮
-@property (strong, nonatomic)       UIButton *collecBtn; // 收藏按钮
-@property (strong, nonatomic)       UIButton *messageBtn; // 消息按钮
-@property (strong, nonatomic)       UIButton *writeBtn; // 写评论
-@property (strong, nonatomic)       UIButton *searchBtn; // 搜索按钮
-@property (strong, nonatomic)       UIImageView *backImage; // 背景图片
+@property (strong, nonatomic)       UIButton *iconImageBtn;//头像按钮
+@property (strong, nonatomic)       UIButton *userNameBtn;//用户名登陆按钮
+@property (strong, nonatomic)       UIButton *downBtn;//下载按钮
+@property (strong, nonatomic)       UIButton *collecBtn;//收藏按钮
+@property (strong, nonatomic)       UIButton *messageBtn;//消息按钮
+@property (strong, nonatomic)       UIButton *writeBtn;//写评论
+@property (strong, nonatomic)       UIButton *searchBtn;//搜索按钮
+@property (strong, nonatomic)       UIImageView *backImage;//背景图片
 
 @end
 
